6f85c6b80411dbac4cad074facb7c94ea9ec6438
Raj Vihari V <93786419+rvudattu@users.noreply.github.com>
2022-12-13 09:08:30 +0530

    Update name.py

--- name.py
+++ name.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/howdy', methods=['POST'])
 def howdy():
     name = request.form['name']
-    return {"res_status": True, "msg":"Git cloud poc."}
+    return {"res_status": True, "msg":"Git cloud poc.."}
   
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port=int(os.environ.get('PORT', 8080)))
